26a8702615facabc12961316778ef0271623c257
AlexisB <misterbalise2@gmail.com>
2024-04-07 19:42:41 +0200

    Add nelmio cors

---
+++ api/api_cinema/src/Service/BuildNewUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class BuildNewUser
+{
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    /**
+     * @param UserPasswordHasherInterface $userPasswordHasher
+     */
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this -> userPasswordHasher = $userPasswordHasher;
+    }
+
+    public function execute(array $parameters):User
+    {
+        $newUser = new User();
+        $newUser -> setEmail($parameters["email"]);
+        $hashedPassword = $this -> userPasswordHasher -> hashPassword($newUser, $parameters["newPassword"]);
+        $newUser -> setPassword($hashedPassword);
+        $newUser -> setRoles(['ROLE_USER']);
+        return $newUser;
+    }
+}
