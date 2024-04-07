f2b24f742d110ae4bef6268faa13fc22fec385b6
AlexisB <misterbalise2@gmail.com>
2024-04-07 12:33:08 +0200

    Add a check to veryfy if an user is unique when created and group attribute for registration

--- api/api_cinema/src/Entity/User.php
+++ api/api_cinema/src/Entity/User.php
@@ -6,25 +6,31 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['create_user'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['create_user'])]
     private ?string $email = null;
 
     /**
      * @var list<string> The user roles
      */
     #[ORM\Column]
+    #[Groups(['create_user'])]
     private array $roles = [];
 
     /**
